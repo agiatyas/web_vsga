945de5789de81992c5eb8aac2bceb0edf6ab7949
Tyas <agiatyas10@gmail.com>
2019-07-12 08:55:39 +0700

    first commit

---
+++ fuction.php
@@ -0,0 +1,18 @@
+<?php 
+	function salam(){
+		return "Selamat Datang, Admin!";
+
+	}
+
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Latihan Function</title>
+</head>
+<body>
+	<h1><?php echo salam(); ?></h1>
+
+</body>
+</html>
